797ed3c26fbf009a7bb618848f2a9db40dccb5af
Robert <127215048+guziq-code@users.noreply.github.com>
2023-03-18 09:28:18 +0100

    Add files via upload

---
+++ DataBase/Produkt.cpp
@@ -0,0 +1,25 @@
+
+// trivial CPP DATA_BASE sample (input, output)
+
+#include "Produkt.h"
+
+int main() {
+
+    Produkt p1;
+    p1.wczytanie();
+    p1.wypisanie();
+    p1.sprawdzenie_dostepnosc();
+
+    Produkt p2;
+    p2.wczytanie();
+    p2.wypisanie();
+    p2.sprawdzenie_dostepnosc();
+
+    Produkt p3;
+    p3.wczytanie();
+    p3.wypisanie();
+    p3.sprawdzenie_dostepnosc();
+
+
+    return 0;
+}
